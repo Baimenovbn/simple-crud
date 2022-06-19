5e7f5b8877cb22610b1fbaa63df11010b4493582
Baimenovbn <baibn@mail.ru>
2022-06-19 09:10:42 +0600

    feat: add internal server error handler

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 import {createServer} from 'http';
+
+import {InternalError} from './errors/internal-error';
+import {handleErrorResponse} from './utils/handle-error-response';
 import {MainController} from './controllers/main.controller';
 
 config();
 
 const server = createServer(async (req, res) => {
-  // try {
+  try {
   await MainController.handleIncomingRequests(req, res);
-  // } catch (e) {
-  //   handleErrorResponse(res, new InternalError());
-  // }
+  } catch (e) {
+    console.error(e);
+    handleErrorResponse(res, new InternalError());
+  }
 });
 
 server.listen(process.env.PORT || 4000, () => {
